af6dc6a453bc3bf00910f89e7d89af1283f5bd41
梅花三十三 <1596944197@qq.com>
2024-10-20 14:14:23 +0800

    feat: nav组件初稿

---
+++ components/Nav.tsx
@@ -0,0 +1,47 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Nav = function EnhancedNav() {
+  const pathname = usePathname(); // Use Next.js navigation hook to get the current path
+  const links = [
+    { href: "/", label: "Learn" },
+    { href: "/timeline", label: "Examples" },
+  ];
+
+  return (
+    <nav className="flex gap-6 flex-wrap items-center justify-center mt-3">
+      {links.map((link) => {
+        const isActive = pathname === link.href;
+
+        return (
+          <Link
+            key={link.href}
+            href={link.href}
+            className={`
+                relative flex items-center gap-2 py-2 px-1
+                text-primary transition-colors duration-200 nav
+                ${isActive ? "text-primary-foreground text-red-500" : ""}
+                hover:scale-105 transition ease-in-out duration-400
+              `}
+            rel="noopener noreferrer"
+          >
+            {link.label}
+            <span
+              className={`
+                  absolute bottom-0 left-0 w-full h-0.5
+                  bg-primary transform transition-transform
+                  ${isActive ? "scale-x-100" : "scale-x-0"}
+                  hover:bg-secondary hover:text-secondary-foreground
+                  transition ease-in-out duration-500
+                  bg-gray-800 origin-center
+                `}
+            />
+          </Link>
+        );
+      })}
+    </nav>
+  );
+};
+
+export default Nav;

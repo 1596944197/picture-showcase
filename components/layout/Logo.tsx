68c8b5697c22f2ae4ee6edcd0ce523bc59d009d7
梅花三十三 <1596944197@qq.com>
2024-10-21 12:19:15 +0800

    feat: logo增加骨架屏

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -1,11 +1,22 @@
-import { Image } from "@nextui-org/react";
+"use client";
+import { Image, Skeleton } from "@nextui-org/react";
 import { NextPage } from "next";
 import Link from "next/link";
+import { useState } from "react";
 
 const Logo: NextPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <Link href="/" className="select-none">
-      <Image src="/logo.svg" alt="Logo" width={36} height={36} />
+      <Skeleton className="w-[36]px h-[36]px" isLoaded={!isLoading}>
+        <Image
+          src="/logo.svg"
+          alt="Logo"
+          width={36}
+          height={36}
+          onLoad={setIsLoading.bind(undefined, false)}
+        />
+      </Skeleton>
     </Link>
   );
 };

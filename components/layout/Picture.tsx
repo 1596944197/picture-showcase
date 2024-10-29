5ddafa5947783686b2a4a897de3d7e64de477272
梅花三十三 <1596944197@qq.com>
2024-10-29 10:14:48 +0800

    refactor: 删除不用代码以及调整图片放大范围

--- components/layout/Picture.tsx
+++ components/layout/Picture.tsx
@@ -27,7 +27,7 @@ const LazyImage = ({ src, alt, onClick }: Props) => {
        w-full sm:w-[45vw] md:w-[30vw] lg:w-[20vw] p-2
      bg-white shadow-xl rounded border border-gray-200 overflow-hidden
      dark:bg-zinc-600 dark:border-zinc-500
-       hover:scale-105 transition-all duration-300
+       hover:scale-110 transition-all duration-300
      "
     >
       <Skeleton className={isLoading ? "h-72" : "h-auto"} isLoaded={!isLoading}>
